da686055b9c2ba4d48d6313648004bb2abfc8966
VilliamVIX <1059448504@qq.com>
2020-10-09 22:03:35 +0800

    权限控制更新

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -3,15 +3,14 @@ const router = express.Router(); // 路由
 const Users = require("../../database/models/Users.js"); //数据库
 import Auth_Jwt from "../../middlewares/auth/Auth_Jwt";
 
-router.post(/^\/vwok/, async (req, res, next) => {
-  let { token } = req.headers;
-
+router.all(/^\/vwok/, async (req, res, next) => {
+  let token  = req.headers.authorization;
   let jwt = new Auth_Jwt(token); // 调用jwt方法
   let isLogin = jwt.Verify_Token(); //校验jwt
 
   console.log(isLogin);
 
-  isLogin == "Been_Login" ? next() : res.status(401).send({ msg: "Not_Login" });
+  isLogin == "Been_Login" ? next() : res.status(401).send({ msg: isLogin });
 
 });
 
